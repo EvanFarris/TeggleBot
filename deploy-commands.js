992022c16504dc95a4999a2237289381426ab30e
EvanFarris <evanfarris0@gmail.com>
2022-04-17 14:30:32 -0500

    Changed file structure according to discordjs.guide

---
+++ deploy-commands.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const fs = require('node:fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9'); 
+const { DISCORD_TOKEN: discordToken, DISCORD_CLIENT_ID: clientID, DISCORD_TEST_SERVER_ID: guildID } = process.env;
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(discordToken);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(Routes.applicationGuildCommands(clientID,guildID), {body: commands},);
+
+	} catch (error) {
+		console.error(error);
+	}
+})();
