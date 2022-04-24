6be0ead7c8f449bb4d799bf05b435ac894643c11
EvanFarris <evanfarris0@gmail.com>
2022-04-24 15:52:06 -0500

    Changed adapter to ngrok for testing. Moved twitchEventSubScribe handlers to its own function (streamerNotifications) and the handlers now correctly send messages to channels

--- commands/tb_deletesubs.js
+++ commands/tb_deletesubs.js
@@ -5,7 +5,9 @@ module.exports = {
 		.setName('tb_deletesubs')
 		.setDescription('Deletes all subscriptions'),
 	async execute(interaction) {
+		console.log("Deleting all eventsub subscriptions");
 		await interaction.client.twitchAPI.eventSub.deleteAllSubscriptions();
+		interaction.reply("Completed.");
 	},
 	
 };
