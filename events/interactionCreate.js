d02d5669a8a9f5fd577e2e02224434da5812132a
EvanFarris <evanfarris0@gmail.com>
2022-04-25 18:43:52 -0500

    Added button implementation to tb_subscribe (and editted interactionCreate.js) to help verify if the person you are subscribing to is correct

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,15 +1,27 @@
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
-		if(!interaction.isCommand()) return;
-		const command = interaction.client.commands.get(interaction.commandName);
-
-		if(!command) return;
-		try {
-			command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			interaction.reply({ content: `There was an error while executing this command!`, ephemeral: true});
+		if(!interaction.isCommand() && !interaction.isButton()) return;
+		if(interaction.isCommand()) {
+			const command = interaction.client.commands.get(interaction.commandName);
+			if(!command) return;
+			try {
+				if(interaction.isCommand()) {
+					command.execute(interaction);
+				} 
+			
+			} catch (error) {
+				console.error(error);
+				interaction.reply({ content: `There was an error while executing this command!`, ephemeral: true});
+			}
+		} else {
+			if(interaction.customId == "tb_subscribe_yes" || interaction.customId == "tb_subscribe_no") {
+				interaction.client.commands.get("tb_subscribe").execute(interaction);
+			}
 		}
+		
+
+		
+		
 	}
 };
