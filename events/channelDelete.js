f426ab301c8ac29e915e73119c56791e15f3a057
EvanFarris <evanfarris0@gmail.com>
2023-01-07 02:14:25 -0600

    forgot to add channelDelete.js to git

---
+++ events/channelDelete.js
@@ -0,0 +1,41 @@
+const {InteractionType} = require(`discord.js`);
+const dbHelper = require(`../helperFiles/database_functions.js`);
+const validationHelper = require(`../helperFiles/validation_functions.js`);
+module.exports = {
+	name: `channelDelete`,
+	execute(channel) {
+		guildDeletedAChannel(channel);
+	}
+};
+
+async function guildDeletedAChannel(channel) {
+	let subscriptions = await dbHelper.getGuildSubsTableEntry(channel);
+
+	if (subscriptions){
+		const streamers = JSON.parse(subscriptions.get(`streamersInfo`));
+		let streamerNames = streamers.names;
+		let streamerWebsites = streamers.websites;
+		let streamerChannels = streamers.channels;
+		let streamerIds = streamers.streamerIds;
+		let initialSubscriptions = subscriptions.get(`numStreamers`);
+		let twitchStreamerEntry = null;
+		for(i = 0; i < streamerNames.length; i++) {
+			if (channel.id == streamerChannels[i]) {
+				twitchStreamerEntry = await validationHelper.checkTwitchStreamerExistsLocal(channel.client, streamerNames[i]);
+				await dbHelper.deleteFollowerFromTwitchStreamer(channel.client, twitchStreamerEntry, streamerIds[i], channel.id);
+				streamerNames.splice(i,1);
+				streamerWebsites.splice(i,1);
+				streamerChannels.splice(i,1);
+				streamerIds.splice(i,1);
+				i--;
+			}
+		}
+		
+		if(streamerNames.length == 0) {
+			await subscriptions.destroy();
+		} else if(initialSubscriptions != streamerNames.length) {
+			const streamersStringified = JSON.stringify({"names" : streamerNames, "websites" : streamerWebsites, "channels" : streamerChannels, "streamerIds" : streamerIds });
+			await channel.client.dbs.guildsubs.update({streamersInfo: streamersStringified, numStreamers : streamerNames.length}, {where: {guildId: `${channel.guildId}`}});
+		}
+	}
+}
