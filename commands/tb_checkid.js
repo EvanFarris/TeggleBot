9c659ba1320b22699d82a0bb58b18165d6d84b99
EvanFarris <evanfarris0@gmail.com>
2023-01-15 01:55:12 -0600

    tb_checkid.js now sends it's response in an embed, and sends the channelId too.

--- commands/tb_checkid.js
+++ commands/tb_checkid.js
@@ -1,11 +1,14 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('discord.js');
+const embedHelper = require(`../helperFiles/embed_helper.js`);
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('tb_checkid')
-		.setDescription(`Get your guildID`),
+		.setDescription(`Get your guild Id and the current channel ID`),
 	async execute(interaction) {
-		await interaction.reply(`${interaction.guildId}`);
+		const information = `Guild Id: ${interaction.guildId}\nChannel Id: ${interaction.channelId}`;
+		const embed = embedHelper.createEmbed(`CheckId results`, information);
+		interaction.reply({embeds: [embed]});
 	},
 	
 };
