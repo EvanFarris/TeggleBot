b6769e5ff3f38dd7ffa0dbeb08f626e66e75ec22
EvanFarris <evanfarris0@gmail.com>
2023-01-20 22:23:28 -0600

    check_id.js did not git added in previous commit

---
+++ commands/check_id.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
+const embedHelper = require(`../helperFiles/embed_functions.js`);
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('check_id')
+		.setDescription(`Get your guild Id and the current channel ID`),
+	async execute(interaction) {
+		const information = `Guild Id: ${interaction.guildId}\nChannel Id: ${interaction.channelId}`;
+		const embed = embedHelper.createEmbed(`CheckId results`, information);
+		interaction.reply({embeds: [embed]});
+	},
+	
+};
